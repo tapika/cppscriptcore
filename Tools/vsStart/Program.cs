89499d9163baa96740e88818352a8d13c8cb8ff6
Tarmo Pikaro <tapika@yahoo.com>
2018-12-15 00:26:27 +0200

    Tune: Add prototype tool for attaching to visual studio, experimental instance using COM

---
+++ Tools/vsStart/Program.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+using EnvDTE;                               //DTE
+using EnvDTE80;
+using Microsoft.VisualStudio.Project.VisualC.VCProjectEngine;
+using Microsoft.VisualStudio.VCProjectEngine;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Management;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static String vsInstallPath = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise";
+    static String devEnvExe = Path.Combine(vsInstallPath, @"Common7\IDE\devenv.exe");
+
+    static void Main(string[] args)
+    {
+        //var procs = System.Diagnostics.Process.GetProcesses().Where(x => x.ProcessName == "devenv").ToArray();
+        //String devenvPath = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe";
+        //Assembly asm = Assembly.LoadFile(devenvPath);
+
+        DTE2 dte = null;
+
+        try
+        {
+
+            //String[] allPaths = {
+            //    Path.Combine(vsInstallPath, @"Common7\IDE\PublicAssemblies"),
+            //    // VCProjectShim is here.
+            //    Path.Combine(vsInstallPath, @"Common7\IDE\CommonExtensions\Microsoft\VC\Project")
+            //};
+
+            //AppDomain.CurrentDomain.AssemblyResolve += (s, asmArgs) =>
+            //{
+            //    String dllName = new AssemblyName(asmArgs.Name).Name + ".dll";
+
+            //    foreach (String dir in allPaths)
+            //    {
+            //        string path = Path.Combine(dir, dllName);
+
+            //        if (!File.Exists(path))
+            //            continue;
+
+            //        return Assembly.LoadFrom(path);
+            //    }
+
+            //    Console.WriteLine("Warning: Required assembly not found: " + dllName);
+            //    return null;
+            //};
+
+            //dte = (DTE)Activator.CreateInstance(Type.GetTypeFromProgID("VisualStudio.DTE.10.0"), true);
+            //dte = (DTE)Activator.CreateInstance(Type.GetTypeFromProgID("VisualStudio.DTE.12.0"), true);
+            //dte = (DTE)Activator.CreateInstance(Type.GetTypeFromProgID("VisualStudio.DTE.16.0"), true);
+
+            //Microsoft.VisualStudio.Shell.ServiceProvider.GlobalProvider.GetService()
+            bool bAttached = false;
+            int processId = 0;
+            EnvDTE.Process processToAttachTo = null;
+
+            for ( int iTry = 0; iTry < 2; iTry++)
+            {
+                dte = (DTE2)Marshal.GetActiveObject("VisualStudio.DTE.15.0");
+
+                var processes = Utils.call(() => (dte.Debugger.LocalProcesses.Cast<EnvDTE.Process>().ToArray() ));
+                foreach (var p in processes)
+                {
+                    String name = Path.GetFileNameWithoutExtension(Utils.call(() => (p.Name))).ToLower();
+                    if (name != "devenv")
+                        continue;
+
+                    processId = Utils.call(() => (p.ProcessID));
+                    String cmdArgs = GetProcessCommandLine(processId);
+
+                    if (!cmdArgs.Contains("-Embedding"))
+                        continue;
+
+                    processToAttachTo = p;
+                    //Console.ReadKey();
+                    Console.WriteLine("Attaching to: " + processId);
+                    Utils.callVoidFunction(() => { processToAttachTo.Attach(); });
+                    bAttached = true;
+                    break;
+                }
+
+                if (bAttached)
+                    break;
+
+                {
+                    if (iTry == 1)
+                    {
+                        Console.WriteLine("Error: Failed to launch vs2017.");
+                        return;
+                    }
+
+                    // Analogue of
+                    // Activator.CreateInstance(Type.GetTypeFromProgID("VisualStudio.DTE.15.0"), true);
+                    // only with  experimental visual studio version.
+                    Start_devenv_Embedded();
+                }
+            }
+
+            dte = (DTE2)GetDTE(processId, 120);
+            //dte = null;
+            String edition = dte.Edition;
+            Console.WriteLine("Edition: " + edition);
+
+            // Make it visible.
+            dte.MainWindow.Visible = true;
+            dte.UserControl = true;
+
+            //if (bAttached)
+            //{
+            //    dte = (DTE2)Marshal.GetActiveObject("VisualStudio.DTE.15.0");
+
+            //    var processes = Utils.call(() => (dte.Debugger.LocalProcesses));
+            //    foreach (var proc in processes)
+            //    {
+            //        EnvDTE.Process p = (EnvDTE.Process)proc;
+            //        int pId = Utils.call(() => (p.ProcessID));
+            //        if (pId != processId)
+            //            continue;
+
+            //        Console.WriteLine("Attaching to: " + processId);
+            //        Utils.callVoidFunction(() => { p.Attach(); });
+            //        bAttached = true;
+            //        //Console.ReadKey();
+            //        break;
+            //    }
+            //}
+
+            //Microsoft.VisualStudio.OLE.Interop.IServiceProvider serv = (Microsoft.VisualStudio.OLE.Interop.IServiceProvider)dte;
+            //dte.ExecuteCommand("File.InvokeOpenSyncProjectFile", "args");
+            //dte.ExecuteCommand("File.InvokeOpenSyncProjectFile");
+            //dte.ExecuteCommand("File.InvokeOpenSyncProjectFile", "thisIsArg1");
+            //dte.ExecuteCommand("Tools.InvokeExecuteScript");
+            //int cmdCount = Utils.call(() => (dte.Commands.Count));
+
+
+            //for( int i = 1; i <= cmdCount; i++)
+            //{
+            //    Command c = Utils.call(() => dte.Commands.Item(i));
+            //    Console.WriteLine(Utils.call(() => c.Name));
+            //    Console.WriteLine( "    " + Utils.call(() => c.ID));
+            //    Console.WriteLine( "    " + Utils.call(() => c.Guid.ToString()) );
+            //    Console.WriteLine();
+            //}
+
+            //Guid service = new Guid("89BE061A-1103-4D1D-8293-A51F8480E202");
+            //Guid serviceApi = new Guid("89BE061A-1103-4D1D-8293-A51F8480E201");
+            //IntPtr obj = IntPtr.Zero;
+            //int r = serv.QueryService(ref service, ref serviceApi, out obj);
+
+            //Console.WriteLine("[ Press any key to close ... ]");
+            //Console.ReadKey();
+
+
+            //Solution sln = dte.Solution;
+            //sln.Open(@"D:\PrototypingQuick\ConsoleApplication2\ConsoleApplication2.sln");
+
+            //Project p = sln.Projects.Item(1);
+            //VCProject vcproj = (VCProject)p.Object;
+            ////VCProject vcproj = (VCProject)p.Object;
+            //Console.WriteLine(vcproj.ProjectFile);
+            //Console.WriteLine(vcproj.ProjectGUID);
+            Console.WriteLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            Console.WriteLine("Stack trace: " + ex.StackTrace);
+            Console.ReadKey();
+        }
+        finally
+        {
+            // Need to close solution, so devenv.exe would not remain hanging in memory.
+            //if (dte != null)
+            //    dte.Solution.Close();
+        }
+    }
+
+    public static int Start_devenv_Embedded()
+    {
+        ProcessStartInfo procStartInfo = new ProcessStartInfo();
+        procStartInfo.Arguments = "-Embedding" + " /RootSuffix Exp";
+        //procStartInfo.Arguments = "-Embedding";
+        procStartInfo.CreateNoWindow = true;
+        procStartInfo.FileName = devEnvExe;
+        procStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        procStartInfo.WorkingDirectory = Environment.CurrentDirectory;
+
+        return System.Diagnostics.Process.Start(procStartInfo).Id;
+    }
+
+    public static string GetProcessCommandLine(int processId)
+    {
+        using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + processId))
+        using (ManagementObjectCollection objects = searcher.Get())
+            return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+    }
+
+    //
+    // Idea borrowed from: https://www.helixoft.com/blog/creating-envdte-dte-for-vs-2017-from-outside-of-the-devenv-exe.html
+    //
+
+
+    /// <summary>
+    /// Gets the DTE object from any devenv process.
+    /// </summary>
+    /// <remarks>
+    /// After starting devenv.exe, the DTE object is not ready. We need to try repeatedly and fail after the
+    /// timeout.
+    /// </remarks>
+    private static DTE GetDTE(int processId, int timeout)
+    {
+        DTE res = null;
+        DateTime startTime = DateTime.Now;
+
+        while (res == null && DateTime.Now.Subtract(startTime).Seconds < timeout)
+        {
+            System.Threading.Thread.Sleep(1000);
+            res = GetDTE(processId);
+        }
+
+        return res;
+    }
+
+
+    [DllImport("ole32.dll")]
+    private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+
+
+    /// <summary>
+    /// Gets the DTE object from any devenv process.
+    /// </summary>
+    /// <param name="processId">
+    /// <returns>
+    /// Retrieved DTE object or <see langword="null"> if not found.
+    /// </see></returns>
+    private static DTE GetDTE(int processId)
+    {
+        object runningObject = null;
+
+        IBindCtx bindCtx = null;
+        IRunningObjectTable rot = null;
+        IEnumMoniker enumMonikers = null;
+
+        try
+        {
+            Marshal.ThrowExceptionForHR(CreateBindCtx(reserved: 0, ppbc: out bindCtx));
+            bindCtx.GetRunningObjectTable(out rot);
+            rot.EnumRunning(out enumMonikers);
+
+            IMoniker[] moniker = new IMoniker[1];
+            IntPtr numberFetched = IntPtr.Zero;
+            while (enumMonikers.Next(1, moniker, numberFetched) == 0)
+            {
+                IMoniker runningObjectMoniker = moniker[0];
+
+                string name = null;
+
+                try
+                {
+                    if (runningObjectMoniker != null)
+                    {
+                        runningObjectMoniker.GetDisplayName(bindCtx, null, out name);
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    // Do nothing, there is something in the ROT that we do not have access to.
+                }
+
+                Regex monikerRegex = new Regex(@"!VisualStudio.DTE\.\d+\.\d+\:" + processId, RegexOptions.IgnoreCase);
+                if (!string.IsNullOrEmpty(name) && monikerRegex.IsMatch(name))
+                {
+                    Marshal.ThrowExceptionForHR(rot.GetObject(runningObjectMoniker, out runningObject));
+                    break;
+                }
+            }
+        }
+        finally
+        {
+            if (enumMonikers != null)
+            {
+                Marshal.ReleaseComObject(enumMonikers);
+            }
+
+            if (rot != null)
+            {
+                Marshal.ReleaseComObject(rot);
+            }
+
+            if (bindCtx != null)
+            {
+                Marshal.ReleaseComObject(bindCtx);
+            }
+        }
+
+        return runningObject as DTE;
+    }
+
+}
+
+
---
+++ Tools/vsStart/Utils.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Thread = System.Threading.Thread;
+
+/// <summary>
+/// Utility functions.
+/// </summary>
+public class Utils
+{
+    /// <summary>
+    /// Calls a function, retrying if necessary.
+    /// </summary><remarks>
+    /// DTE functions call into an instance of Visual Studio running in
+    /// a separate process using COM interop. These calls can fail if 
+    /// Visual Studio is busy, and in these cases we get a COM exception.
+    /// 
+    /// This function will retry calling the function if this happens, and
+    /// will only fail if it has retried 20 times without success.
+    /// 
+    /// You pass in the function - or property - to call usually as a
+    /// lambda. For example, to get the projects.Count property you would 
+    /// call:
+    /// 
+    ///   int count = dteCall[int](() => (projects.Count));
+    ///   
+    /// (Note: replace the [] above with angle-brackets to specify the 
+    ///        return type.)
+    /// </remarks>
+    public static T call<T>(Func<T> fn)
+    {
+        int numTries = 20;
+        int intervalMS = 50;
+
+        // We will try to call the function a number of times...
+        for (int i = 0; i < numTries; ++i)
+        {
+            try
+            {
+                // We call the function passed in and return the result...
+                return fn();
+            }
+            catch (COMException ex)
+            {
+                if ((uint)ex.ErrorCode == 0x8001010a) // RPC_E_SERVERCALL_RETRYLATER: The message filter indicated that the application is busy.
+                {
+                    // Server is busy - we sleep for a short while, and then try again...
+                    Thread.Sleep(intervalMS);
+                }
+                else
+                    throw ex;
+            }
+        }
+
+        throw new Exception(String.Format("'call' failed to call function after {0} tries.", numTries));
+    }
+
+    /// <summary>
+    /// Calls a function with no return value, retrying if necessary.
+    /// </summary>
+    public static void callVoidFunction(Action fn)
+    {
+        int numTries = 20;
+        int intervalMS = 50;
+
+        // We will try to call the function a number of times...
+        for (int i = 0; i < numTries; ++i)
+        {
+            try
+            {
+                // We call the function passed in, and return
+                // if it succeeds...
+                fn();
+                return;
+            }
+            catch (COMException ex)
+            {
+                if ((uint)ex.ErrorCode == 0x8001010a) // RPC_E_SERVERCALL_RETRYLATER: The message filter indicated that the application is busy.
+                {
+                    // Server is busy - we sleep for a short while, and then try again...
+                    Thread.Sleep(intervalMS);
+                }
+                else
+                    throw ex;
+            }
+        }
+
+        throw new Exception(String.Format("'callVoidFunction' failed to call function after {0} tries.", numTries));
+    }
+
+}
+
+
