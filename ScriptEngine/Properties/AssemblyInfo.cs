426e1f521909e20c7fe71a51f080f8f4432bdfce
Tarmo Pikaro <tapika@yahoo.com>
2019-01-14 22:39:29 +0200

    Initial vsix project for script support

---
+++ ScriptEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ScriptEngine")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
