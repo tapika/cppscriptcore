71d39c0806214f8e9150113ead0c3da0fca0d9c7
Tarmo Pikaro <tapika@yahoo.com>
2019-01-15 22:50:22 +0200

    Feature: Add "Link to project..." right click popup menu for solution and project.

--- ScriptEngine/ScriptEnginePackage.cs
+++ ScriptEngine/ScriptEnginePackage.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using Task = System.Threading.Tasks.Task;
 
 namespace ScriptEngine
@@ -11,9 +13,12 @@ namespace ScriptEngine
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(ScriptEnginePackage.PackageGuid)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class ScriptEnginePackage : AsyncPackage
     {
         public const string PackageGuid = "dc06e809-f64c-49e4-b9d5-d6e6d60dee7c";
+        public static readonly Guid CommandSet = new Guid("6fd609b8-64f9-4636-a95b-d3322b87b185");
+        public const int CommandId = 0x0100;
 
         /// <summary>
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
@@ -26,7 +31,20 @@ namespace ScriptEngine
         {
             // Switch to main thread
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            // Register menu handlers
+            OleMenuCommandService commandService = await GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            if (commandService == null)
+                return;
+
+            commandService.AddCommand(new MenuCommand(this.Execute, new CommandID(CommandSet, CommandId)));
         }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            VsShellUtilities.ShowMessageBox(this, "test 1", "title 1", OLEMSGICON.OLEMSGICON_INFO, OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
     }
 }
 
