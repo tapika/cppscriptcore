4474e25f1c407688cba797205d50195aa2dc168c
Tarmo Pikaro <tapika@yahoo.com>
2019-02-03 00:55:08 +0200

    Add initial dll for C++ Solution / project code model

---
+++ SolutionProjectModel/dllmain.cpp
@@ -0,0 +1,9 @@
+#define _WIN32_WINNT  _WIN32_WINNT_WIN7 /*0x0601*/
+#include <SDKDDKVer.h>
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    return TRUE;
+}
+
