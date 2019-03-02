577e02ccb1096dc4863280044f8046e8559a81ac
Tarmo Pikaro <tarmo.pikaro@visualcomponents.com>
2019-03-02 11:35:27 +0200

    Tune: Spell check

--- SolutionProjectModel/Project.h
+++ SolutionProjectModel/Project.h
@@ -76,11 +76,13 @@ public:
     bool Save(const wchar_t* file = nullptr);
 
 protected:
+    //
     //  Visual studio version, in year. e.g. 2017, 2019, ...
+    //
     int vsVersion;
 
     //
-    //  Typically in .vcxproj this is used to park where project is targetted upon, e.g. "Win32Proj" - win32 or win64, "Android", "Linux", "Clang",
+    //  Typically in .vcxproj this is used to mark where project is targetted upon, e.g. "Win32Proj" - win32 or win64, "Android", "Linux", "Clang",
     //  but division of whether it's os, compiler or just .dll inside VS is not so clear. Keeping as string for timebeing.
     //
     std::string Keyword;
