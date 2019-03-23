62f06a25f6827d63fe0ea94e03422c1f0e3f6182
Tarmo Pikaro <tapika@yahoo.com>
2019-03-23 22:32:42 +0200

    Tune: Shorter version, to be consistent with documentation

--- SolutionProjectModel/testCppApp.cpp
+++ SolutionProjectModel/testCppApp.cpp
@@ -8,35 +8,23 @@ void main(void)
 {
     Project p(L"HelloWorld");
 
-    p.SetSaveDirectory(path(__FILE__).parent_path().c_str());
-
-    p.Globals.Keyword = projecttype_Win32Proj;
-
-    p.AddPlatforms( { "Win32", "x64" } );
-    p.AddPlatforms({ "x64" });
-
+    p.SetSaveDirectory(L".");
+    p.AddPlatform("x64");
     p.AddFiles({ L"helloWorld.cpp" });
-
-    //p.AddFiles({ path(p.GetSaveDirectory()).append(L"Solution.cpp").wstring() });
 
     p.VisitConfigurations( 
         [](VCConfiguration& c)
         {
             c.General.IntDir = LR"(obj\$(ProjectName)_$(Configuration)_$(Platform)\)";
             c.General.OutDir = LR"(bin\$(Configuration)_$(Platform)\)";
-            //c.General.TargetName = L"$(ProjectName)";
-            //c.General.TargetExt = ".dll";
             c.General.UseDebugLibraries = true;
             c.General.LinkIncremental = true;
-
             c.CCpp.Optimization = optimization_Disabled;
-
             c.Linker.System.SubSystem = subsystem_Console;
             c.Linker.Debugging.GenerateDebugInformation = debuginfo_true;
         }
     );
 
     p.Save();
-    
 }
 
