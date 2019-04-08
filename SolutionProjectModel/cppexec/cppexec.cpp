cf719bdc9914199610174dd5ad4fd8bbab3a61af
Tarmo Pikaro <tapika@yahoo.com>
2019-04-08 21:53:40 +0300

    Tune: Sketch cppexec main.

--- SolutionProjectModel/cppexec/cppexec.cpp
+++ SolutionProjectModel/cppexec/cppexec.cpp
@@ -1,8 +1,39 @@
-#include "..\Project.h"
+    #include "..\Project.h"
+    #include <filesystem>
+    using namespace std;
+    using namespace filesystem;
 
-int wmain(int argc, wchar_t** argv)
-{
+    int wmain(int argc, wchar_t** argv)
+    {
+        path exePath(argv[0]);
+        auto dir = weakly_canonical(exePath).parent_path();
+        path scriptToRun;
 
-    return 0;
-}
+        for (auto& pit : directory_iterator(dir))
+        {
+            auto& filePath = pit.path();
+            auto ext = filePath.extension();
+
+            if( ext == ".cpp")
+            {
+                if(scriptToRun.empty())
+                {
+                    scriptToRun = filePath;
+                }
+                else
+                {
+                    scriptToRun.clear();
+                    break;
+                }
+            }
+        } //for
+
+        if(scriptToRun.empty())
+        {
+            printf("Usage: %S <.cpp script to run>\r\n", exePath.stem().c_str());
+            return -2;
+        }
+
+        return 0;
+    }
 
