88dbd6b3a3416607ab93f66269af632ac7aa526f
Tarmo Pikaro <tapika@yahoo.com>
2019-04-19 02:01:02 +0300

    Refactor a little bit, extract getInstalledVisualStudios function to be able to query visual studio's and their versions.

--- SolutionProjectModel/cppexec/cppexec.cpp
+++ SolutionProjectModel/cppexec/cppexec.cpp
@@ -1,19 +1,11 @@
 #include "..\Project.h"
-#include "expdef.h"             //printPeExports
-#include "..\helpers.h"
+#include "expdef.h"                 //printPeExports
+#include "..\VisualStudioInfo.h"    //getInstalledVisualStudios
 #include <filesystem>
-#include <atlconv.h>            //CW2A
-#include <algorithm>            //transform
+#include <atlconv.h>                //CW2A
+#include <algorithm>                //transform
 using namespace std;
 using namespace filesystem;
-#include <comdef.h>             //_COM_SMARTPTR_TYPEDEF
-#include <Setup.Configuration.h>
-
-_COM_SMARTPTR_TYPEDEF(ISetupConfiguration, __uuidof(ISetupConfiguration));
-_COM_SMARTPTR_TYPEDEF(IEnumSetupInstances, __uuidof(IEnumSetupInstances));
-_COM_SMARTPTR_TYPEDEF(ISetupInstance, __uuidof(ISetupInstance));
-_COM_SMARTPTR_TYPEDEF(ISetupInstanceCatalog, __uuidof(ISetupInstanceCatalog));
-_COM_SMARTPTR_TYPEDEF(ISetupPropertyStore, __uuidof(ISetupPropertyStore));
 
 class CommandLineArguments : public ReflectClassT<CommandLineArguments>
 {
@@ -43,13 +35,6 @@ string getFileExtension( const wchar_t* filePath )
     transform(ext.begin(), ext.end(), ext.begin(), tolower);
     return ext;
 }
-
-typedef struct
-{
-    int     version;
-    wstring InstallPath;
-}VisualStudioInfo;
-
 
 
 int _wmain(int argc, wchar_t** argv)
@@ -197,56 +182,7 @@ int _wmain(int argc, wchar_t** argv)
         }
     } //if
 
-    vector<VisualStudioInfo> instances;
-
-    // Idea copied from vswhere, except whole implementation was re-written from scratch.
-    {
-        ISetupConfigurationPtr setupCfg;
-        IEnumSetupInstancesPtr enumInstances;
-
-        auto lcid = GetUserDefaultLCID();
-
-        function<void(HRESULT)> hrc = [](HRESULT hr)
-        {
-            if (FAILED(hr))
-            {
-                USES_CONVERSION;
-                throw exception(CW2A(_com_error(hr).ErrorMessage()));
-            }
-        };
-
-        hrc(CoInitialize(nullptr));
-        hrc(setupCfg.CreateInstance(__uuidof(SetupConfiguration)));
-        hrc(setupCfg->EnumInstances(&enumInstances));
-
-        while (true)
-        {
-            ISetupInstance* p = nullptr;
-            unsigned long ul = 0;
-            HRESULT hr = enumInstances->Next(1, &p, &ul);
-            if (hr != S_OK)
-                break;
-
-            ISetupInstancePtr setupi(p, false);
-            ISetupInstanceCatalogPtr instanceCatalog;
-            ISetupPropertyStorePtr store;
-            hrc(setupi->QueryInterface(&instanceCatalog));
-            hrc(instanceCatalog->GetCatalogInfo(&store));
-
-            CComVariant v;
-            hrc(store->GetValue(L"productLineVersion", &v));
-
-            VisualStudioInfo vsinfo;
-            vsinfo.version = atoi(CStringA(v).GetBuffer());
-            CComBSTR instpath;
-            // GetDisplayName can be used to get name of instance.
-            hrc(setupi->GetInstallationPath(&instpath));
-            vsinfo.InstallPath = instpath;
-            instances.push_back(vsinfo);
-        }
-    }
-    CoUninitialize();
-
+    vector<VisualStudioInfo> instances = getInstalledVisualStudios();
     vector<VisualStudioInfo>::iterator it;
 
     if(cmdargs.vs != 0)
